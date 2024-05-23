8f3a5300273e957aca30df6397a59a98ab189bd4
vikram <vikramjassal01@gmail.com>
2024-05-23 15:52:44 +0530

    reastaurent-pro

---
+++ frontend/someapp/src/Recipe/Footer.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footerbox'>
+           
+<div className="footer">
+        <div className="location">
+            <div className="l1"><b>Location</b></div>
+            <div className="l1">121 Einstein Loop N, Bronx</div>
+            <div className="l1"> NY 10475, United States</div>
+            <hr/>
+    </div>
+    <div className="time">
+        <div className="t1"><b>opening hours</b></div>
+        <div className="t1">monday-friday</div>
+        <div className="t1">10am to 5pm</div>
+        <div className="t1">Tel: 010-02-0340</div>
+ 
+    </div>
+    <div className="links">
+        <div className="l2"><b>Useful links</b></div>
+        <div className="l2">Your Account</div>
+        <div className="l2">Become an affiliate</div>
+        <div className="l2">prices</div>
+        <div className="l2">Help</div>
+    </div>
+    <div className="contact">
+    <div className="l2"><b>Contact</b></div>
+        <div className="l2">United States,Pin-45678</div>
+        <div className="l2">restaurent56@gmail.com</div>
+        <div className="l2">+7689998890</div>
+        <div className="l2">+5678902346</div>
+    </div>
+</div>
+<br />
+
+      <div className="container border mt-5">
+        <div className="row">
+            <div className="col mt-4">
+      <p className='p1'><b>Location</b></p>
+<p className='p1'>121 Einstein Loop N, Bronx, NY 10475, United States</p>
+            </div>
+            <div className="col mt-4">
+            <p className='p1'><b>Opening hours</b></p>
+            <p className='p1'>Monday - Friday</p>
+            <p className='p1'>10:00 AM - 08:00 PM</p>
+            <p className='p1'>Tel: 010-02-0340</p>
+            </div>
+            <div className="col mt-4">
+<p className='p1'><b>Useful Links</b></p>
+<p className='p1'>Your Account</p>
+<p className='p1'>Become an affiliate</p>
+<p className='p1'>prices</p>
+<p className='p1'>help</p>
+
+            </div>
+            
+        </div>
+        
+      </div>
+      
+<br />
+<br/><br/>
+<p style={{color:'white'}}>@2023 Copyright:vikramjassal01@gmail.com</p>
+    </div>
+  )
+}
